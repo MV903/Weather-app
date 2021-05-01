baf24d5da530121c17e884059ee7c11830c11319
Mette Vorraa <mettevorraa@gmail.com>
2021-05-01 20:58:00 +0100

    Clean up JS code

--- src/script.js
+++ src/script.js
@@ -1,18 +1,9 @@
+
 let now = new Date();
-function currentTime(response) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+function currentTime() {
   let currentHour = now.getHours();
   let currentMinute = now.getMinutes();
-  let showTime = document.querySelector("#display-time");
-  showTime.innerHTML = `Today ${currentHour}:${currentMinute}`;
+  document.querySelector("#display-time").innerHTML = `Today ${currentHour}:${currentMinute}`;
 }
 currentTime();
 
@@ -20,52 +11,42 @@ function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML =`<div class="row">`;
   let days =["Mon", "Tue", "Wed", "Thu", "Fri"];
-days.forEach(function(day) {
+  days.forEach(function(day) {
   forecastHTML = forecastHTML + `
   <div class="col-2">
-          <div class="forecast-day">${day}</div>
-          <img class="forecast-weather-icon" src="src/images/01d.svg" alt="">
-          <div class="forecast-hi-temp">11°</div>
-          <div class="forecast-lo-temp">6°</div>
-        </div>
+  <div class="forecast-day">${day}</div>
+  <img class="forecast-weather-icon" src="src/images/01d.svg" alt="">
+  <div class="forecast-hi-temp">11°</div>
+  <div class="forecast-lo-temp">6°</div>
+  </div>
   `;});
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
-
+  
 }
 displayForecast();
 
 function displayWeather(response) {
-  console.log(response)
   let temp = Math.round(response.data.main.temp);
   let wind = Math.round(response.data.wind.speed);
   let tempFeels = Math.round(response.data.main.feels_like)
   let description = response.data.weather[0].description;
   let weatherIcon = response.data.weather[0].icon;
-  let showWeatherIcon = document.querySelector("#weather-icon-today");
-  let showTemp = document.querySelector("#temperature");
-  let showWind = document.querySelector("#wind");
-  let showTempFeels = document.querySelector("#feels-like-temp")
-  let showDescription = document.querySelector("#display-weather-description");
-  showTemp.innerHTML = temp;
-  showWind.innerHTML = `${wind} m/s`
-  showTempFeels.innerHTML = tempFeels;
-  showDescription.innerHTML = description;
+  let showWeatherIcon = document.querySelector("#weather-icon-today")
   showWeatherIcon.setAttribute("src", `src/images/${weatherIcon}.svg`);
+  document.querySelector("#temperature").innerHTML = temp;
+  document.querySelector("#wind").innerHTML = wind;
+  document.querySelector("#feels-like-temp").innerHTML = tempFeels;
+  document.querySelector("#display-weather-description").innerHTML = description;
   document.querySelector("#new-city").innerHTML = response.data.name;
 }
 
 function searchCity(event) {
   event.preventDefault();
   let city = document.querySelector("#input-city");
-  let showCity = document.querySelector("#new-city");
   let apiKey = "de31873c66b8933cfbbc1e0df416d91d";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=metric`;
-  if (city.value) {
-    showCity.innerHTML = `${city.value}`;
-  } else {
-    alert("Please enter a city");
-  }
+  document.querySelector("#new-city").innerHTML = `${city.value}`;
   axios.get(apiUrl).then(displayWeather);
 }
 let form = document.querySelector("#search-form");
